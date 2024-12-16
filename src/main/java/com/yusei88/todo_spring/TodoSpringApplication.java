e05a27bd60c68f9f3ad66828f31bcd4a1b2bf26b
yusei88 <yuseisato1@gmail.com>
2024-12-16 22:56:00 +0900

    [Init]初期設定ファイルとテストケースを追加
    SpringInitializerで"Spring Web", "Spring Data JPA", "PostgreSQL Driver", "Spring Boot DevTools", "Lombok"を選択

---
+++ src/main/java/com/yusei88/todo_spring/TodoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.yusei88.todo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yusei88/todo_spring/TodoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusei88.todo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
